dba43babb04d65b00fa936f1ccb1a72022bed583
Kevin Shaferian <kevinshaferian98@gmail.com>
2023-06-21 14:29:45 +0700

    okayhh commit

--- testing1234/ContentView.swift
+++ testing1234/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, w  orld!")
+            Text("Hello, world!")
         }
         .padding()
     }
