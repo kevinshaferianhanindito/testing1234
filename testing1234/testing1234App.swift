cf88fa6a866b7b5be15220fd49efce018154b011
Kevin Shaferian <kevinshaferian98@gmail.com>
2023-06-21 14:28:16 +0700

    Initial Commit

---
+++ testing1234/testing1234App.swift
@@ -0,0 +1,17 @@
+//
+//  testing1234App.swift
+//  testing1234
+//
+//  Created by Kevin Shaferian Hanindito on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct testing1234App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
