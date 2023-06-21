e76452702c48130c950eb41f2862cd5145955d13
Kevin Shaferian <kevinshaferian98@gmail.com>
2023-06-21 14:53:13 +0700

    ok

---
+++ testing1234/onboardingview.swift
@@ -0,0 +1,20 @@
+//
+//  onboardingview.swift
+//  testing1234
+//
+//  Created by Kevin Shaferian Hanindito on 21/06/23.
+//
+
+import SwiftUI
+
+struct onboardingview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct onboardingview_Previews: PreviewProvider {
+    static var previews: some View {
+        onboardingview()
+    }
+}
